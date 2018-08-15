2b2d5b1a6262e304634aa10aeda8f6bc6463030a
Greg Whitfield <grwhitfield@gmail.com>
2018-08-15 21:55:58 +1200

    Initial Commit

---
+++ LuxARiPlayerPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuxARiPlayerPOC
+//
+//  Created by Gregory Whitfield on 15/08/18.
+//  Copyright © 2018 Gregory Whitfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
