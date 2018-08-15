277a7a65657b64063dc941baca408c1d48102d27
Greg Whitfield <grwhitfield@gmail.com>
2018-08-15 23:50:25 +1200

    Basic Location manager functionality implemented to show position etc

--- LuxARiPlayerPOC/ViewController.swift
+++ LuxARiPlayerPOC/ViewController.swift
@@ -7,19 +7,73 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var hAccuracy: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var vAccuracy: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        startLocation = nil
     }
 
+    @IBAction func resetDistance(_ sender: Any) {
+        startLocation = nil
+    }
+    
+    @IBAction func startWhenInUse(_ sender: Any) {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func startAlways(_ sender: Any) {
+        locationManager.stopUpdatingLocation()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager:CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        
+        let latestLocation: CLLocation = locations[locations.count - 1]
+        latitude.text = String(format: "%.4f", latestLocation.coordinate.latitude)
+        longitude.text = String(format: "%.4f", latestLocation.coordinate.longitude)
+        hAccuracy.text = String(format: "%.4f", latestLocation.horizontalAccuracy)
+        altitude.text = String(format: "%.4f", latestLocation.altitude)
+        vAccuracy.text = String(format: "%.4f", latestLocation.verticalAccuracy)
+        
+        if startLocation == nil {
+            startLocation = latestLocation
+        }
+        
+        let distanceBetween: CLLocationDistance = latestLocation.distance(from: startLocation)
+        distance.text = String(format: "%.2f", distanceBetween)
+        
+        print("Latitude \(latestLocation.coordinate.latitude)")
+        print("Longitude = \(latestLocation.coordinate.longitude)")
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 
 }
 
